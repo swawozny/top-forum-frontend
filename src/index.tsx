f91983185f1e1759d0568ea0d48aa3263db1629f
swawozny <sebastianwawony@wp.pl>
2023-09-16 19:47:12 +0200

    Add persisting redux state

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
 import App from "./containers/App";
-import { store } from "./store/store";
 import "./styles/index.css";
+import { persistor, store } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import authReducer from "./slices/authSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer
-  }
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+});
+
+export const persistor = persistStore(store);
